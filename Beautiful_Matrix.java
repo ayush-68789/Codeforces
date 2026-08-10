0d807054efcfff15bea937fe2bd39f1b88e32791
Ayush Chaurasia <ayush.chaurasia68789@gmail.com>
2026-08-10 08:19:26 +0530

    263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A263A

---
+++ Beautiful_Matrix.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Beautiful_Matrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in) ; 
+        int n = 5 ; 
+        int [][] mat = new int[n][n] ;
+        int currR = 0 , currC = 0 ;
+        for(int i = 0 ; i < n ; i ++)
+        {
+            for(int j = 0 ; j < n ; j++)
+            {
+                mat[i][j] = sc.nextInt() ; 
+                if(mat[i][j] == 1)
+                {
+                    currR = i ; 
+                    currC = j ; 
+                }
+            }
+        }
+        int swap = 0  ;
+        while(currR >= 0 && currR < 5 && currC >= 0 && currC < 5 ){
+            if(currR == 2 && currC == 2)
+            {
+                System.out.println(swap);
+                break ;
+            }
+            if(currR < 2)
+            {
+                currR ++ ;
+                swap++ ; 
+            }
+            if(currR > 2)
+            {
+                swap++ ; 
+                currR -- ; 
+            }
+            if(currC < 2)
+            {
+                swap++ ; 
+                currC ++ ; 
+            }
+            if(currC > 2)
+            {
+                swap++ ; 
+                currC -- ; 
+            }
+        }
+    }
+}
